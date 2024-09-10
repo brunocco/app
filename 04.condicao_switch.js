449700cfcefb289f925c4060aa9afc7d694c847f
Bruno Cesar <bruno_cco@hotmail.com>
2024-09-10 18:18:22 -0300

    anotações 4 condição swuitch

---
+++ 04.condicao_switch.js
@@ -0,0 +1,20 @@
+// Estrutura de condição swuitch
+const start = () => {
+    
+    while(true){
+        let opcao = "sair"
+        switch (opcao){
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+                case "listar":
+                    console.log("vamos listar")
+                    break
+                case "sair":
+                    return
+        }
+    }
+
+}
+
+start()
