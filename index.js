5311dd9ca5c93b1fe92fd1bf08615424020ea5ee
Bruno Cesar <bruno_cco@hotmail.com>
2024-09-12 11:16:13 -0300

    implementação do remover metas

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas" + realizadas.length,
+        message: "Metas realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
@@ -79,9 +79,34 @@ const metasAbertas = async () => {
     }
     
     await select({
-        message: "Metas abertas" + abertas.length,
+        message: "Metas abertas: " + abertas.length,
         choices: [...abertas]
     })
+}
+
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false}
+    })
+
+    const itensADeletar = await checkbox ({
+        message: "Selecione um item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+
+    if(itensADeletar == 0){
+        console.log("Nenhum item para deletar")
+        return
+    }
+
+    itensADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    console.log("Meta(s) deleta(s) com sucesso!")
 }
 
 const start = async () => {
@@ -106,7 +131,11 @@ const start = async () => {
                 {
                     name: "Metas abertas",
                     value: "abertas"
-                },                
+                },
+                {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },                                
                 {
                     name: "sair",
                     value: "sair"
@@ -129,6 +158,9 @@ const start = async () => {
             case "abertas":
                 await metasAbertas()
                 break
+            case "deletar":
+                await deletarMetas()
+                break                
             case "sair":
                 console.log("Até a próxima")
                 return
